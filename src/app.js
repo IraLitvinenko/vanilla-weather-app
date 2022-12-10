1efb50ff6d909d974b17d66e9ca16638b1672f0a
IraLitvinenko <84662656+IraLitvinenko@users.noreply.github.com>
2022-12-10 18:39:01 +0200

    added default city weather

--- src/app.js
+++ src/app.js
@@ -86,3 +86,4 @@ fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
 
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
+search("Dnipro");
